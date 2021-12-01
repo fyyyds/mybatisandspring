f84717511598b6baff9bf03d8ee619fce2cbef27
fengye <1324280335@qq.com>
2021-12-01 22:25:37 +0800

    测试提交

--- src/main/java/com/fy/mapper/CustomerMapper.java
+++ src/main/java/com/fy/mapper/CustomerMapper.java
@@ -5,6 +5,6 @@ import com.fy.pojo.Customer;
 public interface CustomerMapper {
     // 通过id查询
     public Customer selectCustomerById(Integer id);
-    // 添加客户
+    // 添加客户123
     void addCustomer(Customer customer);
 }
